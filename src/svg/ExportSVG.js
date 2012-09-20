9054a0ef34fddeadfab44bd18a2f7b63bec1927f
skierons <skierons@stetson.edu>
2012-09-20 16:44:08 -0400

    Refined checkType and exportPath functions.

--- src/svg/ExportSVG.js
+++ src/svg/ExportSVG.js
@@ -82,7 +82,6 @@ var ExportSVG = this.ExportSVG = Base.extend({
 
 		//Getting all of the segments(a point, a HandleIn and a HandleOut) in the path
 		var segArray = path.getSegments();
-
 		var pointArray = new Array();
 		var handleInArray = new Array();
 		var handleOutArray = new Array();
@@ -92,6 +91,53 @@ var ExportSVG = this.ExportSVG = Base.extend({
 			handleInArray[i] = segArray[i].getHandleIn();
 			handleOutArray[i] = segArray[i].getHandleOut();
 		}
+		var type = this._checkType(segArray, pointArray, handleInArray, handleOutArray);
+		switch (type) {
+			case 'rect':
+				svgEle = document.createElementNS(this.NS, 'rect');
+				svgEle.setAttribute('x', pointArray[0].getX());
+				svgEle.setAttribute('y', pointArray[0].getY());
+				//code for rest of rect here
+				break;
+			case'line':
+				svgEle = document.createElementNS(this.NS, 'line');
+				svgEle.setAttribute('x1', pointArray[0].getX());
+				svgEle.setAttribute('y1', pointArray[0].getY());
+				svgEle.setAttribute('x2', pointArray[pointArray.length - 1].getX());
+				svgEle.setAttribute('y2', pointArray[pointArray.length - 1].getY());
+				break;
+			case 'circle':
+				svgEle = document.createElementNS(this.NS, 'circle');
+				var radius = (pointArray[2].getX() - pointArray[0].getX()) /2;
+				var cx = pointArray[0].getX() + radius;
+				svgEle.setAttribute('cx', cx);
+				svgEle.setAttribute('cy', pointArray[0].getY());
+				svgEle.setAttribute('r', radius);
+				break;
+			case 'ellipse':
+				svgEle = document.createElementNS(this.NS, 'ellipse');
+				var radiusX = (pointArray[2].getX() - pointArray[0].getX()) / 2;
+				var radiusY = (pointArray[3].getY() - pointArray[1].getY()) /2;
+				var cx = pointArray[0].getX() + radiusX;
+				svgEle.setAttribute('cx', cx);
+				svgEle.setAttribute('cy', pointArray[0].getY());
+				svgEle.setAttribute('rx', radiusX);
+				svgEle.setAttribute('ry', radiusY);
+				break;
+			case 'polyline':
+				svgEle = document.createElementNS(this.NS, 'polyline');
+				var pointString;
+				for(i = 0; i < pointArray.length; ++i) {
+					pointString += pointArray[i].getX() + ", " + pointArray[i].getY();
+				}
+				svgEle.setAttribute('points', pointString);
+				break;
+			default:
+				svgEle = document.createElementNS(this.NS, 'path');
+				//TODO add attributes to the path element
+		}
+
+
 		//pointstring is formatted in the way the SVG XML will be reading
 		//Namely, a point and the way to traverse to that point
 		var pointString = "";
@@ -128,49 +174,33 @@ var ExportSVG = this.ExportSVG = Base.extend({
 
 			svgPath.setAttribute("stroke-width",path.strokeWidth);
 			return svgPath;
-	},
+	},		
 
 	//TRY TO BREAK THIS! FOR ANDREW
 	_checkType: function(segArray) {
 		var type;
-		var pointArray = new Array();
-		var handleInArray = new Array();
-		var handleOutArray = new Array();
-		var hIY;
-		var hIX;
-		var hOX;
-		var hOY;
 		var dPoint12;
 		var dPoint34;
-		//SCOTT- for this loop, hIX, hIY, hOX and HOY just keep 
-		//getting reset. Like...they're only going to record the 
-		//information for the last segment.
-		for (i = 0; i < segArray.length; i++) {
-			pointArray[i] = segArray[i].getPoint();
-			handleInArray[i] = segArray[i].getHandleIn();
-			handleOutArray[i] = segArray[i].getHandleOut();
-			hIX = handleInArray[i].getX();
-			hIY = handleInArray[i].getY();
-			hOX = handleOutArray[i].getX();
-			hOY = handleOutArray[i].getY();
-		}
 		dPoint12 = pointArray[0].getDistance(pointArray[1], true);
 		dPoint34 = pointArray[2].getDistance(pointArray[3], true);
-		if (hIX === 0 && hIY === 0 && hOX === 0 && hOY === 0) {
-			if (dPoint12 === dPoint34) {
-				type = "rectangle";
-			} else {
-				type = "line";
+		if(segArray.length === 4) {
+			if (hIX === 0 && hIY === 0 && hOX === 0 && hOY === 0) {
+				if (dPoint12 === dPoint34) {
+					type = "rectangle";
+				} 
+			} 
+			else if (handleInArray[0].getX() != 0 || handleInArray[0].getY() != 0 && Math.abs(handleInArray[0].getX()) === Math.abs(handleOutArray[0].getX()) && Math.abs(handleInArray[0].getY()) === Math.abs(handleOutArray[0].getY())) {
+				if (handleInArray[3].getY() === handleOutArray[0].getX() && handleOutArray[3].getY() === handleInArray[0].getX()) {
+					type = 'circle'
+				} else {
+					type = 'ellipse';
+				}
 			}
-		}
-		else if (hIX != 0 ||  hIY != 0 && Math.abs(hIY) === Math.abs(hOY) && Math.abs(hIX) === Math.abs(hOX)) {
-			if (handleInArray[2].getY() === hOX && handleOutArray[2].getY() === hIX) {
-				type = "circle";
-			} else {
-				type = "ellipse";
-			}
-		}
-
-
+		} else if (segArray.length >= 3 && handleInArray[0].getX() === 0 && handleInArray[0].getY() === 0 && handleOutArray[0].getX() === 0 && handleOutArray[0].getY() === 0) {
+			type = 'polyline';
+		} else {
+			type = 'line';
+		} 
+		return type;
 	}
 });
