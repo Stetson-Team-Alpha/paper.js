8304dfcab6b80ae77cb0c8ede0091dbac198414f
jnighlight <jlites@stetson.edu>
2012-09-21 04:04:48 -0400

    Made major imporvements and fixes to ExportSVG I made a new setupPath method for setting up path objects for converstion to SVG, fixed the "path" case in the switch case, added color functionality to all cases, added the curve function in the case of the path case, added a return statement to exportPath, added height and width to rectangles, changed pathExporting so that it focuses on 2 points and the path in between them. It's much more efficient and easy to read, and it works! Added 'curves' variable to the checkType method to clean up the logic a little bit, also redid the if/else statements to make it a lot cleaner looking, seperated first by curves or no curves, the segment number, then the specific cases. Kept the original logic in comments just in case I accidently broke something. Also cleaned up some syntax justin brought to our attention, exchanging '===' for '==' and "string" to 'string'. Unfortunately, we're getting an "Unexpected identifier" error for the two methods Scott and I entered, so I was unable to test them. I believe there's a rather quick fix for that that I hope to discover in the team meeting tomorrow.

--- src/svg/ExportSVG.js
+++ src/svg/ExportSVG.js
@@ -78,7 +78,7 @@ var ExportSVG = this.ExportSVG = Base.extend({
 	 * returns svg object (xml dom)
 	 */
 	exportPath: function(path) {
-		var svgPath = document.createElementNS(this.NS, 'path');
+		var svgEle;
 
 		//Getting all of the segments(a point, a HandleIn and a HandleOut) in the path
 		var segArray = path.getSegments();
@@ -91,13 +91,16 @@ var ExportSVG = this.ExportSVG = Base.extend({
 			handleInArray[i] = segArray[i].getHandleIn();
 			handleOutArray[i] = segArray[i].getHandleOut();
 		}
-		var type = this._checkType(segArray, pointArray, handleInArray, handleOutArray);
+		var type = 'path' //this._checkType(segArray, pointArray, handleInArray, handleOutArray);
 		switch (type) {
 			case 'rect':
 				svgEle = document.createElementNS(this.NS, 'rect');
 				svgEle.setAttribute('x', pointArray[0].getX());
 				svgEle.setAttribute('y', pointArray[0].getY());
+				svgEle.setAttribute('width', pointArray[1].getDistance(pointArray[2], true));
+				svgEle.setAttribute('height', pointArray[0].getDistance(pointArray[1], true));
 				//code for rest of rect here
+				//Rect still lacks rounded corners functionality. Necessary?
 				break;
 			case'line':
 				svgEle = document.createElementNS(this.NS, 'line');
@@ -140,60 +143,138 @@ var ExportSVG = this.ExportSVG = Base.extend({
 				}
 				svgEle.setAttribute('points', pointString);
 				break;
+			//Do we need a text case?
 			default:
+				//Is there a more efficient way to do this...?(See ln 175) -Jacob
 				svgEle = document.createElementNS(this.NS, 'path');
-				//TODO add attributes to the path element
+				//svgEle = this.pathSetup(path);
+				break;
 		}
+		if (path.strokeColor != undefined) {
+			svgEle.setAttribute('stroke', path.strokeColor.toCssString());
+		}
+		if (path.fillColor != undefined) {
+			svgEle.setAttribute('fill', path.fillColor.toCssString());
+		}
+		if(path.strokeWidth != undefined){
+			svgEle.setAttribute('stroke-width', path.strokeWidth);
+		}
+		if(path.name != undefined) {
+			svgEle.setAttribute('name', path.name);
+		}
+		return svgEle;
+	}
 
 
 		//pointstring is formatted in the way the SVG XML will be reading
 		//Namely, a point and the way to traverse to that point
-		var pointString = "";
-		for (i = 0; i < pointArray.length; i++) {
-			var x = pointArray[i].getX();
-			//x = x - (x % 1); //Possible for simplifying  
-			var y = pointArray[i].getY();
-			//y = y - (x % 1);
-			if (i === 0) {
-				//M is moveto, moving to a point without drawing
-				pointString+= "M " + x + " " + y + " ";
-			} else {
-				//L is lineto, moving to a point with drawing
-				pointString+= "L " + x + " " + y + " ";
+		//GETTING UNEXPECTED IDENTIFIER HERE...WHY?
+		/*pathSetup: function(path) {
+			var svgPath = document.createElementNS(this.NS, 'path');
+			var segArray = path.getSegments();
+			var pointArray = new Array();
+			var handleInArray = new Array();
+			var handleOutArray = new Array();
+			
+			//...because this block of code is repeated from a previous method. (lns 88 - 93)
+			for (i = 0; i < segArray.length; i++) {
+				pointArray[i] = segArray[i].getPoint();
+				hIArray[i] = segArray[i].getHandleIn();
+				hOArray[i] = segArray[i].getHandleOut();
+			}
+			var pointString = '';
+			var x1;
+			var x2;
+			var y1;
+			var y2;
+			var handleOut1;
+			var handleIn2;
+			//Checks 2 points and the angles in between the 2 points
+			for (i = 0; i < pointArray.length-1; i++) {
+				x1 = pointArray[i].getX();
+				y1 = pointArray[i].getY();
+				x2 = pointArray[i + 1].getX();
+				y2 = pointArray[i + 1].getY();
+				handleOut1 = hOarray[i];
+				handleIn2 = hIarray[i+1];
+				if(handleOut1 == 0 && handleIn2 == 0){
+					if (i == 0) {
+						//M is moveto, moving to a point without drawing
+						pointString+= 'M ' + x2 + ' ' + y2 + ' ';
+					} else {
+						//L is lineto, moving to a point with drawing
+						pointString+= 'L ' + x2 + ' ' + y2 + ' ';
+				}
+				} else {
+					//c is curveto, relative: handleOut, handleIn - endpoint, endpoint - startpoint
+					pointString+= 'c ' + handleOut1.getX() + ' ' + handleOut1.getY() + ' ';
+					pointString+= (handleIn2.getX() - x2) + ' ' + (handleIn2.getY() - y2) + ' ';
+					pointString+= (x2 - x1) + ' ' + (y2-y1) +  ' ';
 			}
 		}
-		if (path.getClosed())
+			if (path.getClosed())
 			{
 				//Z implies a closed path, connecting the first and last points
-				pointString += "z";
+				pointString += 'z';
 			}
-
-			svgPath.setAttribute("d",pointString);
-
-			//If the stroke doesn't have a color, there's no attribute for it
-			if (path.strokeColor != undefined) {
-				svgPath.setAttribute("stroke", path.strokeColor.toCssString());
-			}
-
-			//If the fill doesn't have a color, there's no attribute for it
-			if (path.fillColor != undefined) {
-				svgPath.setAttribute("fill", path.fillColor.toCssString());
-			}
-
-			svgPath.setAttribute("stroke-width",path.strokeWidth);
+			svgPath.setAttribute('d',pointString);
 			return svgPath;
-	},		
+	},	*/	
 
 	//TRY TO BREAK THIS! FOR ANDREW
-	_checkType: function(segArray) {
+	//Need to add path functionality
+	//UNEXPECTED IDENTIFIER HERE AS WELL. WHY?
+	/*_checkType: function(segArray) {
 		var type;
 		var dPoint12;
 		var dPoint34;
 		dPoint12 = pointArray[0].getDistance(pointArray[1], true);
 		dPoint34 = pointArray[2].getDistance(pointArray[3], true);
-		if(segArray.length === 4) {
-			if (hIX === 0 && hIY === 0 && hOX === 0 && hOY === 0) {
-				if (dPoint12 === dPoint34) {
+		var curves = false
+		var segHandleIn;
+		var segHandleOut;
+		for( var i in segArray){
+			//Checks for any curves. Differentiates between straight things(line, polyline, rect) and
+			// curvy things  (circle, ellipse, path).
+			segHandleIn = segArray[i].getHandleIn();
+			segHandleOut = segArray[i].getHandleOut();
+			curves = segHandleIn.getX() != 0 || segHandleIn.getY() != 0 ? true : curves;
+			curves = segHandleOut().getX() != 0 || segHandleOut().getY() != 0 ? true : curves;			
+		}
+		//Cleaned up the logic so it's a little easier to read and includes path.
+		//Kept the old logic for reference. Also, I kept the really long logic lines in their correct
+		//places, because I couldn't figure them out completely.
+		if(curves){
+			if(segArray.length == 4) {
+				if(handleInArray[0].getX() != 0 || handleInArray[0].getY() != 0 && Math.abs(handleInArray[0].getX()) === Math.abs(handleOutArray[0].getX()) && Math.abs(handleInArray[0].getY()) === Math.abs(handleOutArray[0].getY())) {
+					if(handleInArray[3].getY() === handleOutArray[0].getX() && handleOutArray[3].getY() === handleInArray[0].getX()) {
+						type = 'circle';
+					} else {
+						type = 'ellipse';
+					}
+					
+				}
+			} else {
+				type = 'path';
+			}
+		} else {
+			if(segArray.length == 4 && dPoint12 == dPoint34) {
+				type = 'rectangle';
+			} else if(segArray.length >= 3) {
+				if(path.getClosed()) {
+					type = 'polygon';
+				} else {
+					type = 'polyline';
+			} else {
+				type = 'line';
+			}
+			
+		}
+		
+		
+		/*if(segArray.length == 4) {
+			if (!curves) {
+				if (dPoint12 == dPoint34) {
 					type = "rectangle";
 				} 
 			} 
@@ -204,15 +285,16 @@ var ExportSVG = this.ExportSVG = Base.extend({
 					type = 'ellipse';
 				}
 			}
-		} else if (segArray.length >= 3 && handleInArray[0].getX() === 0 && handleInArray[0].getY() === 0 && handleOutArray[0].getX() === 0 && handleOutArray[0].getY() === 0) {
+		} else if (segArray.length >= 3 && !curves{
 			if(path.getClosed()) {
 				type = 'polygon';
 			} else {
 				type = 'polyline';
 			}
-		} else {
+		} else if(segArray.length == 2 && !curves){
 			type = 'line';
-		} 
+		}/*
+		
 		return type;
-	}
+	}*/
 });
